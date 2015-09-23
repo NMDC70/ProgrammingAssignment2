95d1e9b38bcc540d27cb8fd53f00b43a04798943
NMDC70 <nagesh.madhwal@gmail.com>
2015-09-23 22:52:28 +0800

    Added Text for how the program works

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,9 @@
 ## write a pair of functions that cache the inverse of a matrix
+##--------
+## picking up from the example given for the assignment, the steps for the solution program
+## are replicated in the same manner, with the same decription. The working of the 
+## program is explained at the bottom.
+##=======
 
 ## `makeCacheMatrix`: This function creates a special "matrix" object that can cache its inverse
 ## Thefunction, `makeCacheMatrix` creates a special "vector", which is a list containing a function to
@@ -36,12 +41,13 @@ cacheSolve <- function(x, ...) {
   m
   }
 ## HOW THE SOLUTION COMES TOGETHER
-## First time we run something like a <- makeCacheMatrix (matrix(6:9, nrow = 2, ncol = 2))
+## First time we run commond like:
+##> a <- makeCacheMatrix (matrix(6:9, nrow = 2, ncol = 2))
 ## m is programatically set to null. When cacheSolve(a) is called the inverse is calculated
 ## This inverse is now stored in m. Everytime now when we call cacheSolve(a) the inverse stored 
 ## in m is returned without having to compute the inverse. 
-## if a new matrix is assigned to makeCacheMatrix, m is reset to null & so the first time the inverse 
-## will again be computed and its value will be assigned to m.
+## if a new matrix is assigned to makeCacheMatrix, m is reset to null & so when cacheSolve(a) is 
+## called it will again computed the inverse and assigned it to m.
 ## a$setmatrix will set an inverse value which will alwayed be returned by cacheSolve as the value
 ## from a$setmatrix is assigned to m. But whenever a new matrix is created with makeCacheMatrix the
 ## value of m is again set to null & the new inverse is calculated by cacheSolve and assigned to m.
